e661935c2d6c0a9de12474cdbe540b7e7c42a3d2
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-09-14 12:22:32 -0300

    alteração

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 20;
+        b = 20;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        
+
+            System.out.println(resultado);
+        }
+    }
+
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -7,14 +7,10 @@ public class TiposVariaveis {
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+        numero = 10;
 
-       int numero =5;
-       numero = 10;
-
-       System.out.println(numero);
-
-       final double VALOR_DE_PI = 3.14;
+        System.out.println(numero);
 
     }
 }
