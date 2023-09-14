9ebc654728763c93827e65d0599c253d6912dd23
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-09-14 15:38:18 -0300

    Ffm das variaveis

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,24 +1,43 @@
+// public class Operadores {
+//     public static void main(String[] args) {
+//         // int a, b;
+
+//         // a = 20;
+//         // b = 20;
+
+//         // String resultado = a==b ? "Verdadeiro" : "Falso";
+
+//         // System.out.println(resultado);
+
+//         int numero1 = 1;
+//         int numero2 = 2;
+
+//         boolean simNao = numero1 == numero2;
+
+//         System.out.println(simNao);
+
+//         simNao = numero1 != numero2;
+
+//         System.out.println("pé ralé " + simNao);
+
+//     }
+// }
+
 public class Operadores {
     public static void main(String[] args) {
-        // int a, b;
 
-        // a = 20;
-        // b = 20;
+        boolean condicao1 = true;
 
-        // String resultado = a==b ? "Verdadeiro" : "Falso";
+        boolean condicao2 = false;
 
-        // System.out.println(resultado);
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
 
-        int numero1 = 1;
-        int numero2 = 2;
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
 
-        boolean simNao = numero1 == numero2;
-
-        System.out.println(simNao);
-
-        simNao = numero1 != numero2;
-
-        System.out.println("pé ralé " + simNao);
-
+        }
+        System.out.println("Fim");
     }
 }
