7c2b26dabdc75cc227781cfa14fee412970eef29
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-09-14 14:42:35 -0300

    atualizar

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,24 @@
 public class Operadores {
     public static void main(String[] args) {
-        int a, b;
+        // int a, b;
 
-        a = 20;
-        b = 20;
+        // a = 20;
+        // b = 20;
 
-        String resultado = a==b ? "Verdadeiro" : "Falso";
+        // String resultado = a==b ? "Verdadeiro" : "Falso";
 
-        
+        // System.out.println(resultado);
 
-            System.out.println(resultado);
-        }
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("pé ralé " + simNao);
+
     }
-
+}
