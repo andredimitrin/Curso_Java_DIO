68b19281b10035fa1a3bbeccc735292e3b8eb623
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-10-03 11:53:38 -0300

    nova

---
+++ estrutura_repeticao/ExemploFor.java
@@ -0,0 +1,13 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println(carneirinhos + " carneirinhos");
+
+            if (carneirinhos == 12)
+                break;
+
+        }
+        System.out.println("Pegou no sono... ");
+        System.out.println("ZzZzZz ZZZzZz");
+    }
+}
