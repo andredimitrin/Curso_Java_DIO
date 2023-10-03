aa875ab10eb14ece9736b327718e018d838c9ca1
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-10-03 11:37:55 -0300

    att

--- estrutura_condicional/src/SistemaMedia.java
+++ estrutura_condicional/src/SistemaMedia.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class SwitchCase {
+// public class SistemaMedia {
+//     public static void main(String[] args) {
+
+//         String sigla = "M";
+
+//         if (sigla == "P")
+//             System.out.println("Pequeno");
+//         else if (sigla == "M")
+//             System.out.println("Médio");
+//         else if (sigla == "G")
+//             System.out.println("Grande");
+//         else
+//             System.out.println("Inválido");
+
+//     }
+// }
+
+public class SistemaMedia {
+    public static void main(String[] args) {
+
+        String sigla = "M";
+
+        switch (sigla) {
+            case "P":
+                System.out.println("Pequeno");
+                break;
+            case "M":
+                System.out.println("Médio");
+                break;
+            case "G":
+                System.out.println("Grande");
+                break;
+            default:
+                System.out.println("Inválido");
+                break;
+        }
+    }
 }
